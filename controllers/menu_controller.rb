712db5ea963a2e092baa7fed25b8570f46106773
Virginia <vcooper_us@yahoo.com>
2015-07-21 01:11:12 -0700

    First commit with failed code.

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -14,28 +14,28 @@ def main_menu
 
     puts "Main Menu - #{@address_book.entries.count} entries"
     puts "1 - View all entries"
-<<<<<<< HEAD
+    ##<<<<<<< HEAD
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
     puts "5 - Exit"
-=======
+    ##=======
     puts "2 - View entry number n"
     puts "3 - Create an entry"
     puts "4 - Search for an entry"
     puts "5 - Import entries from a CSV"
     puts "6 - Exit"
->>>>>>> Checkpoint-22-Menu
+    ##>>>>>>> Checkpoint-22-Menu
     print "Enter your selection: "
 
 # # 3
 
     selection = gets.to_i
-<<<<<<< HEAD
-    puts "You picked #{selection}"
-    end
-end
-=======
+    ##<<<<<<< HEAD
+    ##puts "You picked #{selection}"
+    ##end
+    ##end
+##=======
     
 # # 7
     case selection
@@ -74,7 +74,7 @@ end
             main_menu
     end
 end
-
+##end
 
 # # 10
 
@@ -148,9 +148,84 @@ end
         end
 
         def search_entries
+            ## 9
+            print "Search by name: "
+            name = gets.chomp
+            
+            ## 10
+            match = @address_book.binary_search(name)
+            system "clear"
+            
+            ## 11
+            if match
+                puts match.to_s
+                search_submenu(match)
+            
+            else
+                puts "No match found for #{name}"
+            end
+            
         end
 
+        def search_submenu(entry)
+            ## 12
+            puts "d - delete entry"
+            puts "e - edit this entry"
+            puts "m - return to main menu"
+
+            ## 13
+            selection = gets.chomp
+            
+            
+            ## 14
+                case selection
+                    when "d"
+                        system "clear"
+                        delete_entry(entry)
+                        main_menu
+                    
+                    when "e"
+                        edit_entry(entry)
+                        system "clear"
+                        main_menu
+                    
+                    when "m"
+                        system "clear"
+                        main_menu
+                    
+                    else
+                        system "clear"
+                        puts "#{selection} is not a valid input"
+                        puts entry.to_s
+                        search_submenu(entry)
+                    end
+        end
+
+                    
+
         def read_csv
+            
+            ## 1
+            print "Enter CSV file to import: "
+            file_name = gets.chomp
+            
+            ## 2
+            if file_name.empty?
+                system "clear"
+                puts "No CSV file read"
+                main_menu
+            end
+            
+            ## 3
+            begin
+                entry_count = @address_book.import_from_csv(file_name).count
+                system "clear"
+                puts "#{entry_count} new entries added from #{file_name}"
+            rescue
+                puts "#{file_name} is not valid CSV file, please enter the name of a valid CSV file"
+                read_csv
+            end
+            
         end
         
         def entry_submenu(entry)
@@ -172,7 +247,13 @@ end
         when "n"
 # # 19
         when "d"
+        ## 7
+            delete_entry(entry)
+            
         when "e"
+        ## 8
+            edit_entry(entry)
+            entry_submenu(entry)
 # # 20
         when "m"
             system "clear"
@@ -184,6 +265,32 @@ end
             entries_submenu(entry)
         end
 
+        end
+
+        def delete_entry(entry)
+            @address_book.entries.delete(entry)
+            puts "#{entry.name} has been deleted"
+        end
+
+        def edit_entry(entry)
+            ## 4
+                print "Updated name: "
+                name = gets.chomp
+                print "Updated phone number: "
+                phone_number = gets.chomp
+                print "Updated email: "
+                email = gets.chomp
+                
+            ## 5
+                entry.name = name if !name.empty?
+                entry.phone_number = phone_number if !phone_number.empty?
+                entry.email = email if !email.empty?
+                system "clear"
+                
+                ## 6
+                puts "Updated entry: "
+                puts entry
+        end
+
 end
-end
->>>>>>> Checkpoint-22-Menu
+##>>>>>>> Checkpoint-22-Menu
